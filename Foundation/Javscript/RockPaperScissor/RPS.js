e1bef06332ddb4392a2bad381fac8886f5bb7ed2
Jake Tran <jake.tranehs@gmail.com>
2021-09-25 03:01:54 -0700

    Complete play one round function for RPS project

--- Foundation/Javscript/RockPaperScissor/RPS.js
+++ Foundation/Javscript/RockPaperScissor/RPS.js
@@ -1,11 +1,27 @@
-const RPSArr = ["Rock", "Paper", "Scissor"];
+const RPSArr = ["rock", "paper", "scissor"];
 
 function getRandomSelection(choices) {
   let random = Math.floor(Math.random() * choices.length);
   return choices[random];
 }
 
-function playOne(RPSArr) {
-  let playerSelection = getRandomSelection(RPSArr);
-  let computerSelection = getRandomSelection(RPSArr);
+function playOne(choices) {
+  let playerSelection = prompt("Enter Rock, Paper, or Scissors").toLowerCase();
+  let computerSelection = getRandomSelection(choices);
+
+  console.log(playerSelection, computerSelection)
+
+  if (playerSelection == computerSelection) {
+    return "Draw";
+  } else if (playerSelection == "rock" && computerSelection == "scissor") {
+    return "Player";
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return "Player";
+  } else if (playerSelection == "scissor" && computerSelection == "paper") {
+    return "Player";
+  } else {
+    return "Computer";
+  }
 }
+
+console.log(playOne(RPSArr));
