ab5f18d9bd45b61b9e20f2dbcc2671414880517b
Jake Tran <jake.tranehs@gmail.com>
2021-10-10 02:40:10 -0700

    Complete sumAll exercise

--- Foundation/javascript-exercises-main/05_sumAll/sumAll.js
+++ Foundation/javascript-exercises-main/05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+  if (num1 < 0 || num2 < 0 || typeof num1 != 'number' || typeof num2 != 'number') {
+    return 'ERROR'
+  }
+
+  let startNumber = isOneLarger(num1, num2) ? num2 : num1
+  let endNumber = isOneLarger(num1, num2) ? num1 : num2
+  
+
+  return calculateSumAll(startNumber, endNumber)
 };
+
+// HELPER FUNCTIONS
+
+function isOneLarger(num1, num2) {
+  return num1 > num2
+}
+
+function calculateSumAll(start, end) {
+
+  let sum = start
+
+  while (start < end) {
+    sum = sum + (start+1)
+    start++
+  }
+
+  return sum
+}
 
 // Do not edit below this line
 module.exports = sumAll;
