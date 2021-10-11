0160a0a048da4260be8e620ddd0a27ddb1ab9fb2
Jake Tran <jake.tranehs@gmail.com>
2021-10-11 14:21:50 -0700

    Completed leapYear exercise

--- Foundation/javascript-exercises-main/05_sumAll/sumAll.js
+++ Foundation/javascript-exercises-main/05_sumAll/sumAll.js
@@ -1,32 +1,34 @@
-const sumAll = function(num1, num2) {
-
-  if (num1 < 0 || num2 < 0 || typeof num1 != 'number' || typeof num2 != 'number') {
-    return 'ERROR'
+const sumAll = function (num1, num2) {
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    typeof num1 != "number" ||
+    typeof num2 != "number"
+  ) {
+    return "ERROR";
   }
 
-  let startNumber = isOneLarger(num1, num2) ? num2 : num1
-  let endNumber = isOneLarger(num1, num2) ? num1 : num2
-  
+  let startNumber = isOneLarger(num1, num2) ? num2 : num1;
+  let endNumber = isOneLarger(num1, num2) ? num1 : num2;
 
-  return calculateSumAll(startNumber, endNumber)
+  return calculateSumAll(startNumber, endNumber);
 };
 
 // HELPER FUNCTIONS
 
 function isOneLarger(num1, num2) {
-  return num1 > num2
+  return num1 > num2;
 }
 
 function calculateSumAll(start, end) {
-
-  let sum = start
+  let sum = start;
 
   while (start < end) {
-    sum = sum + (start+1)
-    start++
+    sum = sum + (start + 1);
+    start++;
   }
 
-  return sum
+  return sum;
 }
 
 // Do not edit below this line
