9fb8eb112513d2d82366a93396dc5083d6e71187
Jake Tran <jake.tranehs@gmail.com>
2021-10-13 14:29:56 -0700

    Complete tempConversion exercise

--- Foundation/javascript-exercises-main/07_tempConversion/tempConversion.js
+++ Foundation/javascript-exercises-main/07_tempConversion/tempConversion.js
@@ -1,4 +1,15 @@
+// toFix will round the number to nearest 10th and parseFloat will get rid of trailing 0 if the number happen
+// to be whole number
 
+const ftoc = function (fahrenheit) {
+  let celsiusConverted = ((fahrenheit - 32) * 0.5556).toFixed(1);
+  return parseFloat(celsiusConverted);
+};
+
+const ctof = function (celsius) {
+  let fahrenheitConverted = (celsius * 1.8 + 32).toFixed(1);
+  return parseFloat(fahrenheitConverted);
+};
 
 // Do not edit below this line
 module.exports = {
