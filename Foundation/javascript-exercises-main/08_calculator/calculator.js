ceebca89f148d82babcbb99e2ca52afd54b0d443
Jake Tran <jake.tranehs@gmail.com>
2021-10-15 14:59:43 -0700

    Complete calculator exercise

--- Foundation/javascript-exercises-main/08_calculator/calculator.js
+++ Foundation/javascript-exercises-main/08_calculator/calculator.js
@@ -1,25 +1,59 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let arrLength = arr.length;
+
+  if (arrLength < 2) {
+    if (arrLength == 0) {
+      return 0;
+    }
+    return arr[0];
+  }
+
+  let sum = 0;
+
+  for (let i = 0; i < arrLength; i++) {
+    sum += arr[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function (arr) {
+  let arrLength = arr.length;
 
+  if (arrLength < 2) {
+    if (arrLength == 0) {
+      return 0;
+    }
+    return arr[0];
+  }
+
+  let product = 1;
+
+  for (let i = 0; i < arrLength; i++) {
+    product *= arr[i];
+  }
+
+  return product;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num == 0 || num == 1) {
+    return 1;
+  }
+
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +63,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
