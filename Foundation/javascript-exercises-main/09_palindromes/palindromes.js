2a4d4a06fa4fa4ea17a4e19af316b775b7b8b90b
Jake Tran <jake.tranehs@gmail.com>
2021-10-17 16:39:31 -0700

    Complete Palindrome exercise

--- Foundation/javascript-exercises-main/09_palindromes/palindromes.js
+++ Foundation/javascript-exercises-main/09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+  str = str.toLowerCase()
+
+  // Regex below will replace everything except for A-Z a-z and 0-9 with empty '',
+  // g is for global and not first match
+  let cleanedStr = str.replace(/[^A-Za-z0-9]/g, "");
+
+  let strArr = cleanedStr.split("");
+  // We're using slice() here to get a shallow copy before reversing bc
+  // reverse() method will apply on the original itself and doesnt create a copy
+  let reverseStr = strArr.slice().reverse();
+
+  return (strArr.join() == reverseStr.join())
 };
 
 // Do not edit below this line
