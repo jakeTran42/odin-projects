81858b77b07ae078da1974673447e67066ddc7f7
Jake Tran <jake.tranehs@gmail.com>
2021-10-19 18:51:34 -0700

    COmplete fib sequence exercise

--- Foundation/javascript-exercises-main/10_fibonacci/fibonacci.js
+++ Foundation/javascript-exercises-main/10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (fib) {
+  if (fib < 0) {
+    return "OOPS";
+  } else if (fib == 0) {
+    return 0;
+  } else if (fib == 1 || fib == 2) {
+    return 1;
+  }
 
+  let fib1 = 1;
+  let fib2 = 1;
+  let sum = 0;
+
+  for (let i = 3; i <= fib; i++) {
+    sum = fib1 + fib2;
+    fib1 = fib2;
+    fib2 = sum;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
