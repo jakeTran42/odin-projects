73fc5191913c2e2f86fb457606e119e1fe5f1243
Jake Tran <jake.tranehs@gmail.com>
2021-10-20 19:16:04 -0700

    Complete getTheTitle exercise

--- Foundation/javascript-exercises-main/11_getTheTitles/getTheTitles.js
+++ Foundation/javascript-exercises-main/11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function (objArr) {
+  let titleArr = [];
 
+  for (let i = 0; i < objArr.length; i++) {
+    titleArr.push(objArr[i].title);
+  }
+
+  return titleArr;
 };
 
 // Do not edit below this line
