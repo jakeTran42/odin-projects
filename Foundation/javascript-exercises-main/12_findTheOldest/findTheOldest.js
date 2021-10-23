0ca1a4cc7ada285aebf74c9c1dda65fd0efd0125
Jake Tran <jake.tranehs@gmail.com>
2021-10-22 23:55:00 -0700

    Complete findTheOldest exercise

--- Foundation/javascript-exercises-main/12_findTheOldest/findTheOldest.js
+++ Foundation/javascript-exercises-main/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (objArr) {
+  let currentOldestPerson = undefined;
+  let currentObjAge = 0;
+  let currentYear = 2022;
 
+  for (let i = 0; i < objArr.length; i++) {
+    currentObjAge =
+      (objArr[i].yearOfDeath ? objArr[i].yearOfDeath : currentYear) -
+      objArr[i].yearOfBirth;
+
+    if (currentOldestPerson == undefined) {
+      currentOldestPerson = { ...objArr[i], age: currentObjAge };
+    } else {
+      if (currentOldestPerson.age < currentObjAge) {
+        currentOldestPerson = { ...objArr[i], age: currentObjAge };
+      }
+    }
+  }
+
+  return currentOldestPerson;
 };
 
 // Do not edit below this line
