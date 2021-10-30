28170670cdd5251208665a3c1527f019d44c071a
Jake Tran <jake.tranehs@gmail.com>
2021-10-30 02:43:06 -0700

    Finish exercise 3 DOM manipulation

--- Foundation/Javascript/DOM/DOM-manipulation-practice.js
+++ Foundation/Javascript/DOM/DOM-manipulation-practice.js
@@ -20,11 +20,12 @@ container.appendChild(blueHeader);
 // another <h1> that says “I’m in a div”
 // a <p> that says “ME TOO!”
 // Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.
-// const div = document.createElement('div');
-// div.style.cssText = 'border-color: black; background: pink;';
-// const divChildH1 = document.createElement('h1');
-// divChildH1.textContent("I'm in a div");
-// const divChildP = document.createElement('p');
-// divChildP.textContent("ME TOO!");
-// div.appendChild(divChildH1, divChildP);
-// container.appendChild(div);
+const div = document.createElement('div');
+div.style.cssText = 'border: 1px solid black; background: pink;';
+const divChildH1 = document.createElement('h1');
+divChildH1.textContent = "I'm in a div";
+const divChildP = document.createElement('p');
+divChildP.textContent = "ME TOO!";
+div.appendChild(divChildH1);
+div.appendChild(divChildP);
+container.appendChild(div);
